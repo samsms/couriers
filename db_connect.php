f7c1d4d58aff68d0cad2d5d42a88f543bac9b15a
samsms <you@example.com>
2022-09-05 01:43:41 +0300

    auto push

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php 
 if($_SERVER['REMOTE_ADDR']!="127.0.0.1"){
 
-$conn= new mysqli('localhost','techsava_rivercourt_property','Trymenot#123$','techsava_courier')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','techsava_courier','Trymenot#123$','techsava_courier')or die("Could not connect to mysql".mysqli_error($con));
 }else{
     
 $conn= new mysqli('localhost','root','Trymenot#123$','techsava_courier')or die("Could not connect to mysql".mysqli_error($con));
